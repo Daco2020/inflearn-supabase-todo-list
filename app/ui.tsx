999c4ad6cdc0cd417cbbffa64d0126fe4b790f03
Daco2020 <dacokim32@gmail.com>
2024-08-01 17:24:16 +0900

    feat: CRUD 구현

--- app/ui.tsx
+++ app/ui.tsx
@@ -1,26 +1,48 @@
-'use client';
+"use client";
 
 import { Button, Input } from "@material-tailwind/react";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { createTodo, getTodos } from "actions/todo-actions";
 import Todo from "components/todo";
+import { useState } from "react";
 
 export default function UI() {
-    return (
-        <div className="w-2/3 mx-auto flex flex-col items-center py-10">
-        <h1 className="text-xl">TODO LIST</h1>
+  const [searchInput, setSearchInput] = useState("");
 
-        <Input
-         label="Search TODO" 
-         placeholder="Search TODO" 
-         icon={<i className="fas fa-search" />}
-        /> 
+  const todosQuery = useQuery({
+    queryKey: ["todos"],
+    queryFn: () => getTodos({ searchInput }),
+  });
 
-        <Todo />
+  const createTodoMutation = useMutation({
+    mutationFn: () => createTodo({ title: "New TODO", completed: false }),
+    onSuccess: () => {
+      todosQuery.refetch();
+    },
+  });
 
-        <Button>
-            <i className="fas fa-plus mr-2"></i>
-            Add TODO
-        </Button>
+  return (
+    <div className="w-2/3 mx-auto flex flex-col items-center py-10">
+      <h1 className="text-xl">TODO LIST</h1>
 
-        </div>
-    );
+      <Input
+        label="Search TODO"
+        placeholder="Search TODO"
+        icon={<i className="fas fa-search" />}
+        value={searchInput}
+        onChange={(e) => setSearchInput(e.target.value)}
+      />
+
+      {todosQuery.isPending && <p>Loading...</p>}
+      {todosQuery.data &&
+        todosQuery.data.map((todo) => <Todo key={todo.id} todo={todo} />)}
+      <Button
+        onClick={() => createTodoMutation.mutate()}
+        loading={createTodoMutation.isPending}
+      >
+        <i className="fas fa-plus mr-2"></i>
+        Add TODO
+      </Button>
+    </div>
+  );
 }
--- components/todo.tsx
+++ components/todo.tsx
@@ -1,44 +1,85 @@
 "use client";
 
 // 체크박스 컴포넌트를 불러옵니다.
-import { Checkbox, IconButton } from "@material-tailwind/react";
+import { Checkbox, IconButton, Spinner } from "@material-tailwind/react";
+import { useMutation } from "@tanstack/react-query";
+import { deleteTodo, updateTodo } from "actions/todo-actions";
+import { queryClient } from "config/ReactQueryClientProvider";
 import { useState } from "react";
 
-export default function Todo({}) {
-    const [isEditing, setIsEditing] = useState(false);
-    const [completed , setCompleted] = useState(false);
-    const [title, setTitle] = useState("");
+export default function Todo({ todo }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [completed, setCompleted] = useState(todo.completed);
+  const [title, setTitle] = useState(todo.title);
 
-    return (
-        <div className="w-full flex items-center gap-1">
-        <Checkbox checked={completed} onChange={e => setCompleted(e.target.checked)}/> 
+  const updateTodoMutaion = useMutation({
+    mutationFn: () =>
+      updateTodo({
+        id: todo.id,
+        title,
+        completed,
+      }),
+    onSuccess: () => {
+      setIsEditing(false);
+      queryClient.invalidateQueries({
+        queryKey: ["todos"],
+      });
+    },
+  });
 
-        {isEditing ? (
-            <input 
-            className="flex-1 border-b-black border-b-1 pb-1"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)} 
-            />
-        ) : (
-            <p className={`flex-1 ${completed && 'line-through'}`}>{title}</p>
-        )}
+  const deleteTodoMutation = useMutation({
+    mutationFn: () => deleteTodo(todo.id),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["todos"],
+      });
+    },
+  });
 
-        {
-            isEditing ? (
-                <IconButton
-                onClick={() => setIsEditing(false)}>
-                    <i className="fas fa-check"></i>
-                </IconButton>
-            ) : (
-                <IconButton
-                onClick={() => setIsEditing(true)}>
-                    <i className="fas fa-pen"></i>
-                </IconButton>
-            )
-        }
-        <IconButton>
-            <i className="fas fa-trash"></i>
+  return (
+    <div className="w-full flex items-center gap-1">
+      <Checkbox
+        checked={completed}
+        onChange={async (e) => {
+          await setCompleted(e.target.checked);
+          await updateTodoMutaion.mutate();
+        }}
+      />
+
+      {isEditing ? (
+        <input
+          className="flex-1 border-b-black border-b pb-1"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      ) : (
+        <p className={`flex-1 ${completed && "line-through"}`}>{title}</p>
+      )}
+
+      {isEditing ? (
+        <IconButton
+          onClick={async () => {
+            await updateTodoMutaion.mutate();
+          }}
+        >
+          {updateTodoMutaion.isPending ? (
+            <Spinner />
+          ) : (
+            <i className="fas fa-check"></i>
+          )}
         </IconButton>
-        </div>
-    );
+      ) : (
+        <IconButton onClick={() => setIsEditing(true)}>
+          <i className="fas fa-pen"></i>
+        </IconButton>
+      )}
+      <IconButton onClick={() => deleteTodoMutation.mutate()}>
+        {deleteTodoMutation.isPending ? (
+          <Spinner />
+        ) : (
+          <i className="fas fa-trash"></i>
+        )}
+      </IconButton>
+    </div>
+  );
 }
